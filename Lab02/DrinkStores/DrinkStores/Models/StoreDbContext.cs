882a202defb3b72b90d8b350e2ea36719482b175
an <54616933+tranthian-snake@users.noreply.github.com>
2020-11-16 18:27:29 +0700

    EAP

---
+++ Lab02/DrinkStores/DrinkStores/Models/StoreDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DrinkStores.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
